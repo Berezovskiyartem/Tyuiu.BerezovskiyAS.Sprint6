893930709d5e4b69f47a8b27bd06263a226328d9
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 10:03:30 +0500

    Task4

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task4.V25/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task4.V25
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
