8f8282b30bd0410f0b26160791a3ccc218f5d7c3
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 09:54:25 +0500

    Task3

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task3.V16.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BerezovskiyAS.Sprint6.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task3.V16/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task3.V16
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
