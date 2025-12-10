14a153b8c7bdbfdc424209be1c629e99d40e984c
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 08:04:17 +0500

    Add project files.

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint6.Task0.V4.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task0.V4.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            double res = ds.Calculate(x);
+            double wait = 68.3;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task0.V4/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task0.V4
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
