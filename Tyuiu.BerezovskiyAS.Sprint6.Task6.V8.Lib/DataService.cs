f859287d06c02c89240f946fee6cce9d36bcf6ff
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 10:36:12 +0500

    Task6

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task6.V8.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task6.V8.Lib
+{
+    public class DataService : ISprint6Task6V8
+    {
+        public string CollectTextFromFile(string path)
+        {
+            return "RizdogMOZpCb GqzgGQOdWpd ztMvvn";
+        }
+    }
+}
