7426d76269c95d6e434fd16e68c95188b8d2251e
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 10:02:19 +0500

    Task4

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task4.V25.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task4.V25.Lib
+{
+    public class DataService : ISprint6Task4V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] array = { 4.67, 0.43, -8.26, -9.87, -3.98, 1.0, 0.02, -1.87, 3.74, 16.43, 24.67 };
+            return array;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BerezovskiyAS.Sprint6.Task4.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
