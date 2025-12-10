58b63de24cd7f5f9b6058cc053ceb7cd25d97ea6
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-12-10 09:11:11 +0500

    Task1

---
+++ Tyuiu.BerezovskiyAS.Sprint6.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint6.Task1.V7.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint6.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
